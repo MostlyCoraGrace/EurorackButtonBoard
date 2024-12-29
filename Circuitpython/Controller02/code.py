72afe3230995ee600f3617b9f2250de6437a86c8
Cora <contact@jhgrace.com>
2024-12-29 16:13:13 +1100

    code

---
+++ Circuitpython/Controller02/code.py
@@ -0,0 +1,35 @@
+"""
+JoystickXL Example #1 - Start Simple (2 axes, 2 buttons, 1 hat switch).
+
+Tested on an Adafruit ItsyBitsy M4 Express, but should work on other CircuitPython
+boards with a sufficient quantity/type of pins.
+
+* Buttons are on pins D9 and D10
+* Axes are on pins A2 and A3
+* Hat switch is on pins D2 (up), D3 (down), D4 (left) and D7 (right)
+
+Don't forget to copy boot.py from the example folder to your CIRCUITPY drive.
+"""
+
+import board  # type: ignore (this is a CircuitPython built-in)
+from joystick_xl.inputs import Axis, Button, Hat
+from joystick_xl.joystick import Joystick
+
+joystick = Joystick()
+
+joystick.add_input(
+    Button(board.D0),
+    Button(board.D1),
+    Button(board.SDA),
+    Button(board.SCL),
+    Button(board.D2),
+    Button(board.MISO),
+    Axis(board.A0),
+    Axis(board.A1),
+    Axis(board.A2),
+    Axis(board.A3),
+    Axis(board.A4),
+)
+
+while True:
+    joystick.update()
