7362d40c3d6c5339e7ca7906adfc0b80ac326265
Cora <contact@jhgrace.com>
2024-12-29 17:54:47 +1100

    code progress. added more buttons, and figured out rotary encoders

--- Circuitpython/Controller01/code.py
+++ Circuitpython/Controller01/code.py
@@ -1,35 +1,52 @@
-"""
-JoystickXL Example #1 - Start Simple (2 axes, 2 buttons, 1 hat switch).
-
-Tested on an Adafruit ItsyBitsy M4 Express, but should work on other CircuitPython
-boards with a sufficient quantity/type of pins.
-
-* Buttons are on pins D9 and D10
-* Axes are on pins A2 and A3
-* Hat switch is on pins D2 (up), D3 (down), D4 (left) and D7 (right)
-
-Don't forget to copy boot.py from the example folder to your CIRCUITPY drive.
-"""
-
-import board  # type: ignore (this is a CircuitPython built-in)
+import board
+import rotaryio
 from joystick_xl.inputs import Axis, Button, Hat
 from joystick_xl.joystick import Joystick
 
+#Set up the joystick
 joystick = Joystick()
 
 joystick.add_input(
-    Button(board.D0),
-    Button(board.D1),
-    Button(board.SDA),
-    Button(board.SCL),
-    Button(board.D2),
-    Button(board.MISO),
-    Axis(board.A0),
-    Axis(board.A1),
-    Axis(board.A2),
-    Axis(board.A3),
-    Axis(board.A4),
+    Button(board.D0), # Large momentary switch
+    Button(board.D1), # Large two-way momentary switch A
+    Button(board.SDA), # Large two-way momentary switch B
+    Button(board.SCL), # Large toggle switch
+    Button(board.D2), # Small toggle switch
+    Button(board.MISO), # Big missile switch
+    Axis(board.A0), # Big joystick X axis
+    Axis(board.A1), # Big joystick Y axis
+    Axis(board.A2), # Little slider axis
+    Axis(board.A3), # Large 1x knob Left
+    Axis(board.A4), # Large 1x knob right
+    Button(board.MOSI), # left Rotary encoder middle click
+    Button(), # left Rotary encoder A
+    Button(), # left Rotary encoder B
 )
 
+# Set up the rotary encoder inputs
+ENC_A = board.A5
+ENC_B = board.SCK
+
+encoder = rotaryio.IncrementalEncoder(ENC_A, ENC_B)
+encoder_pos = encoder.position
+last_encoder_pos = encoder.position
+encoder_value = 0
+
 while True:
+    # The encoder needs to be set manually
+    encoder_pos = encoder.position # Get the current encoder position
+    encoder_value = encoder_pos - last_encoder_pos # how does the encoder position differ
+    if not encoder_value == 0:
+        if encoder_value > 0:
+            joystick.button[7].source_value = 1
+            joystick.button[8].source_value = 0
+        else:
+            joystick.button[7].source_value = 0
+            joystick.button[8].source_value = 1
+    else:
+        joystick.button[7].source_value = 1
+        joystick.button[8].source_value = 1
+    last_encoder_pos = encoder.position # Store the current encoder position for next time
+    
+    # Now we can continue
     joystick.update()
